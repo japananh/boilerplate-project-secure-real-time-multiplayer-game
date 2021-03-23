e76056e0e4ab977c35c598ea34f1c9825952efed
Tran Nhat Anh <japananh@gmail.com>
2021-03-23 20:45:05 +0700

    done

--- server.js
+++ server.js
@@ -1,48 +1,87 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const expect = require('chai');
-const socket = require('socket.io');
-
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner.js');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+// const expect = require("chai");
+const socket = require("socket.io");
+const helmet = require("helmet");
+const http = require("http");
+const nocache = require("nocache");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner.js");
 
 const app = express();
+const server = http.createServer(app);
+socket.listen(server);
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/assets', express.static(process.cwd() + '/assets'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/assets", express.static(process.cwd() + "/assets"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(nocache());
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "http://localhost:3000",
+          "https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap",
+        ],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "http://localhost:3000",
+          "https://code.jquery.com/jquery-2.2.1.min.js",
+          "https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap",
+        ],
+      },
+    },
+    xssFilter: true,
+    hidePoweredBy: false,
+    nocache: true,
+    frameguard: {
+      action: "sameorigin",
+    },
+    referrerPolicy: {
+      policy: "same-origin",
+    },
+  })
+);
+
+app.use(function (_req, res, next) {
+  res.setHeader("X-Powered-By", "PHP 7.4.3");
+  next();
+});
+
 // Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
-    
+
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-const server = app.listen(portNum, () => {
+server.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch (error) {
-        console.log('Tests are not valid:');
+        console.log("Tests are not valid:");
         console.error(error);
       }
     }, 1500);
