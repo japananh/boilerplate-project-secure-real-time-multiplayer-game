40e67b4c5620e8bd03783162b1c0f1670a2b880f
Tran Nhat Anh <japananh@gmail.com>
2021-03-23 22:30:31 +0700

    fix ui

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner.js");
 
 const app = express();
-const server = http.createServer(app);
-socket.listen(server);
+// const server = http.createServer(app);
+// const io = socket.listen(server);
 
 app.use(
   helmet({
@@ -47,7 +47,7 @@ app.use(function (req, res, next) {
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-server.listen(portNum, () => {
+const server = app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
   if (process.env.NODE_ENV === "test") {
     console.log("Running Tests...");
@@ -60,6 +60,116 @@ server.listen(portNum, () => {
       }
     }, 1500);
   }
+});
+
+// Socket.io setup:
+// Start app and bind
+// Socket.io to the same port
+const io = socket(server);
+const Collectible = require("./public/Collectible");
+const { generateStartPos, canvasCalcs } = require("./public/canvas-data");
+
+let currPlayers = [];
+const destroyedCoins = [];
+
+const generateCoin = () => {
+  const rand = Math.random();
+  let coinValue;
+
+  if (rand < 0.6) {
+    coinValue = 1;
+  } else if (rand < 0.85) {
+    coinValue = 2;
+  } else {
+    coinValue = 3;
+  }
+
+  return new Collectible({
+    x: generateStartPos(
+      canvasCalcs.playFieldMinX,
+      canvasCalcs.playFieldMaxX,
+      5
+    ),
+    y: generateStartPos(
+      canvasCalcs.playFieldMinY,
+      canvasCalcs.playFieldMaxY,
+      5
+    ),
+    value: coinValue,
+    id: Date.now(),
+  });
+};
+
+let coin = generateCoin();
+
+io.sockets.on("connection", (socket) => {
+  console.log(`New connection ${socket.id}`);
+
+  socket.emit("init", { id: socket.id, players: currPlayers, coin });
+
+  socket.on("new-player", (obj) => {
+    obj.id = socket.id;
+    currPlayers.push(obj);
+    socket.broadcast.emit("new-player", obj);
+  });
+
+  socket.on("move-player", (dir, obj) => {
+    const movingPlayer = currPlayers.find((player) => player.id === socket.id);
+    if (movingPlayer) {
+      movingPlayer.x = obj.x;
+      movingPlayer.y = obj.y;
+
+      socket.broadcast.emit("move-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: movingPlayer.x, y: movingPlayer.y },
+      });
+    }
+  });
+
+  socket.on("stop-player", (dir, obj) => {
+    const stoppingPlayer = currPlayers.find(
+      (player) => player.id === socket.id
+    );
+    if (stoppingPlayer) {
+      stoppingPlayer.x = obj.x;
+      stoppingPlayer.y = obj.y;
+
+      socket.broadcast.emit("stop-player", {
+        id: socket.id,
+        dir,
+        posObj: { x: stoppingPlayer.x, y: stoppingPlayer.y },
+      });
+    }
+  });
+
+  socket.on("destroy-item", ({ playerId, coinValue, coinId }) => {
+    if (!destroyedCoins.includes(coinId)) {
+      const scoringPlayer = currPlayers.find((obj) => obj.id === playerId);
+      const sock = io.sockets.connected[scoringPlayer.id];
+
+      scoringPlayer.score += coinValue;
+      destroyedCoins.push(coinId);
+
+      // Broadcast to all players when someone scores
+      io.emit("update-player", scoringPlayer);
+
+      // Communicate win state and broadcast losses
+      if (scoringPlayer.score >= 100) {
+        sock.emit("end-game", "win");
+        sock.broadcast.emit("end-game", "lose");
+      }
+
+      // Generate new coin and send it to all players
+      coin = generateCoin();
+      io.emit("new-coin", coin);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("remove-player", socket.id);
+    currPlayers = currPlayers.filter((player) => player.id !== socket.id);
+  });
 });
 
 module.exports = app; // For testing
